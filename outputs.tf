4703950047d447a61274703f5cb51465c90a5b03
Daniel Allen <dan@aeternagroup.net>
2026-01-11 00:49:49 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "app_configurations" {
+  description = "All app_configuration resources"
+  value       = azurerm_app_configuration.app_configurations
+}
+
---
+++ variables.tf
@@ -0,0 +1,54 @@
+variable "app_configurations" {
+  description = <<EOT
+Map of app_configurations, attributes below
+Required:
+    - location
+    - name
+    - resource_group_name
+Optional:
+    - data_plane_proxy_authentication_mode
+    - data_plane_proxy_private_link_delegation_enabled
+    - local_auth_enabled
+    - public_network_access
+    - purge_protection_enabled
+    - sku
+    - soft_delete_retention_days
+    - tags
+    - encryption (block):
+        - identity_client_id (optional)
+        - key_vault_key_identifier (optional)
+    - identity (block):
+        - identity_ids (optional)
+        - type (required)
+    - replica (block):
+        - location (required)
+        - name (required)
+EOT
+
+  type = map(object({
+    location                                         = string
+    name                                             = string
+    resource_group_name                              = string
+    data_plane_proxy_authentication_mode             = optional(string, "Local")
+    data_plane_proxy_private_link_delegation_enabled = optional(bool, false)
+    local_auth_enabled                               = optional(bool, true)
+    public_network_access                            = optional(string)
+    purge_protection_enabled                         = optional(bool, false)
+    sku                                              = optional(string, "free")
+    soft_delete_retention_days                       = optional(number, 7)
+    tags                                             = optional(map(string))
+    encryption = optional(object({
+      identity_client_id       = optional(string)
+      key_vault_key_identifier = optional(string)
+    }))
+    identity = optional(object({
+      identity_ids = optional(set(string))
+      type         = string
+    }))
+    replica = optional(object({
+      location = string
+      name     = string
+    }))
+  }))
+}
+
