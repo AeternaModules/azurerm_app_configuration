6796f1e187e393ede0809888644aaefd77831717
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:15:40 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,81 +1,77 @@
-output "app_configurations" {
-  description = "All app_configuration resources"
-  value       = azurerm_app_configuration.app_configurations
-}
 output "app_configurations_data_plane_proxy_authentication_mode" {
-  description = "List of data_plane_proxy_authentication_mode values across all app_configurations"
-  value       = [for k, v in azurerm_app_configuration.app_configurations : v.data_plane_proxy_authentication_mode]
+  description = "Map of data_plane_proxy_authentication_mode values across all app_configurations, keyed the same as var.app_configurations"
+  value       = { for k, v in azurerm_app_configuration.app_configurations : k => v.data_plane_proxy_authentication_mode }
 }
 output "app_configurations_data_plane_proxy_private_link_delegation_enabled" {
-  description = "List of data_plane_proxy_private_link_delegation_enabled values across all app_configurations"
-  value       = [for k, v in azurerm_app_configuration.app_configurations : v.data_plane_proxy_private_link_delegation_enabled]
+  description = "Map of data_plane_proxy_private_link_delegation_enabled values across all app_configurations, keyed the same as var.app_configurations"
+  value       = { for k, v in azurerm_app_configuration.app_configurations : k => v.data_plane_proxy_private_link_delegation_enabled }
 }
 output "app_configurations_encryption" {
-  description = "List of encryption values across all app_configurations"
-  value       = [for k, v in azurerm_app_configuration.app_configurations : v.encryption]
+  description = "Map of encryption values across all app_configurations, keyed the same as var.app_configurations"
+  value       = { for k, v in azurerm_app_configuration.app_configurations : k => v.encryption }
 }
 output "app_configurations_endpoint" {
-  description = "List of endpoint values across all app_configurations"
-  value       = [for k, v in azurerm_app_configuration.app_configurations : v.endpoint]
+  description = "Map of endpoint values across all app_configurations, keyed the same as var.app_configurations"
+  value       = { for k, v in azurerm_app_configuration.app_configurations : k => v.endpoint }
 }
 output "app_configurations_identity" {
-  description = "List of identity values across all app_configurations"
-  value       = [for k, v in azurerm_app_configuration.app_configurations : v.identity]
+  description = "Map of identity values across all app_configurations, keyed the same as var.app_configurations"
+  value       = { for k, v in azurerm_app_configuration.app_configurations : k => v.identity }
 }
 output "app_configurations_local_auth_enabled" {
-  description = "List of local_auth_enabled values across all app_configurations"
-  value       = [for k, v in azurerm_app_configuration.app_configurations : v.local_auth_enabled]
+  description = "Map of local_auth_enabled values across all app_configurations, keyed the same as var.app_configurations"
+  value       = { for k, v in azurerm_app_configuration.app_configurations : k => v.local_auth_enabled }
 }
 output "app_configurations_location" {
-  description = "List of location values across all app_configurations"
-  value       = [for k, v in azurerm_app_configuration.app_configurations : v.location]
+  description = "Map of location values across all app_configurations, keyed the same as var.app_configurations"
+  value       = { for k, v in azurerm_app_configuration.app_configurations : k => v.location }
 }
 output "app_configurations_name" {
-  description = "List of name values across all app_configurations"
-  value       = [for k, v in azurerm_app_configuration.app_configurations : v.name]
+  description = "Map of name values across all app_configurations, keyed the same as var.app_configurations"
+  value       = { for k, v in azurerm_app_configuration.app_configurations : k => v.name }
 }
 output "app_configurations_primary_read_key" {
-  description = "List of primary_read_key values across all app_configurations"
-  value       = [for k, v in azurerm_app_configuration.app_configurations : v.primary_read_key]
+  description = "Map of primary_read_key values across all app_configurations, keyed the same as var.app_configurations"
+  value       = { for k, v in azurerm_app_configuration.app_configurations : k => v.primary_read_key }
 }
 output "app_configurations_primary_write_key" {
-  description = "List of primary_write_key values across all app_configurations"
-  value       = [for k, v in azurerm_app_configuration.app_configurations : v.primary_write_key]
+  description = "Map of primary_write_key values across all app_configurations, keyed the same as var.app_configurations"
+  value       = { for k, v in azurerm_app_configuration.app_configurations : k => v.primary_write_key }
 }
 output "app_configurations_public_network_access" {
-  description = "List of public_network_access values across all app_configurations"
-  value       = [for k, v in azurerm_app_configuration.app_configurations : v.public_network_access]
+  description = "Map of public_network_access values across all app_configurations, keyed the same as var.app_configurations"
+  value       = { for k, v in azurerm_app_configuration.app_configurations : k => v.public_network_access }
 }
 output "app_configurations_purge_protection_enabled" {
-  description = "List of purge_protection_enabled values across all app_configurations"
-  value       = [for k, v in azurerm_app_configuration.app_configurations : v.purge_protection_enabled]
+  description = "Map of purge_protection_enabled values across all app_configurations, keyed the same as var.app_configurations"
+  value       = { for k, v in azurerm_app_configuration.app_configurations : k => v.purge_protection_enabled }
 }
 output "app_configurations_replica" {
-  description = "List of replica values across all app_configurations"
-  value       = [for k, v in azurerm_app_configuration.app_configurations : v.replica]
+  description = "Map of replica values across all app_configurations, keyed the same as var.app_configurations"
+  value       = { for k, v in azurerm_app_configuration.app_configurations : k => v.replica }
 }
 output "app_configurations_resource_group_name" {
-  description = "List of resource_group_name values across all app_configurations"
-  value       = [for k, v in azurerm_app_configuration.app_configurations : v.resource_group_name]
+  description = "Map of resource_group_name values across all app_configurations, keyed the same as var.app_configurations"
+  value       = { for k, v in azurerm_app_configuration.app_configurations : k => v.resource_group_name }
 }
 output "app_configurations_secondary_read_key" {
-  description = "List of secondary_read_key values across all app_configurations"
-  value       = [for k, v in azurerm_app_configuration.app_configurations : v.secondary_read_key]
+  description = "Map of secondary_read_key values across all app_configurations, keyed the same as var.app_configurations"
+  value       = { for k, v in azurerm_app_configuration.app_configurations : k => v.secondary_read_key }
 }
 output "app_configurations_secondary_write_key" {
-  description = "List of secondary_write_key values across all app_configurations"
-  value       = [for k, v in azurerm_app_configuration.app_configurations : v.secondary_write_key]
+  description = "Map of secondary_write_key values across all app_configurations, keyed the same as var.app_configurations"
+  value       = { for k, v in azurerm_app_configuration.app_configurations : k => v.secondary_write_key }
 }
 output "app_configurations_sku" {
-  description = "List of sku values across all app_configurations"
-  value       = [for k, v in azurerm_app_configuration.app_configurations : v.sku]
+  description = "Map of sku values across all app_configurations, keyed the same as var.app_configurations"
+  value       = { for k, v in azurerm_app_configuration.app_configurations : k => v.sku }
 }
 output "app_configurations_soft_delete_retention_days" {
-  description = "List of soft_delete_retention_days values across all app_configurations"
-  value       = [for k, v in azurerm_app_configuration.app_configurations : v.soft_delete_retention_days]
+  description = "Map of soft_delete_retention_days values across all app_configurations, keyed the same as var.app_configurations"
+  value       = { for k, v in azurerm_app_configuration.app_configurations : k => v.soft_delete_retention_days }
 }
 output "app_configurations_tags" {
-  description = "List of tags values across all app_configurations"
-  value       = [for k, v in azurerm_app_configuration.app_configurations : v.tags]
+  description = "Map of tags values across all app_configurations, keyed the same as var.app_configurations"
+  value       = { for k, v in azurerm_app_configuration.app_configurations : k => v.tags }
 }
 
